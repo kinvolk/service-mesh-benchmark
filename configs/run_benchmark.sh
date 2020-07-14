c8cfb7c08981cfa2520d8719f7b5199ae12876af
Thilo Fromm <thilo@kinvolk.io>
2020-07-14 17:52:13 +0200

    configs: add yaml template for wrk2, script to render template

--- configs/run_benchmark.sh
+++ configs/run_benchmark.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 pushgw_base=http://pushgateway.monitoring:9091/metrics/job/wrk2_bench_test
-connections="$((32*3))"
-duration="120"
+CONNECTIONS="$((32*3))"
+DURATION="120"
 
 app="$1"
 case "$app" in
     emojivoto) 
-        pushgw=$pushgw_base/instance/emojivoto
-        rps=25000
+        PUSHGW=$pushgw_base/instance/emojivoto
+        RPS=25000
         app_instance_count=$(kubectl get namespaces | grep emojivoto | wc -l)
         ;;
     bookinfo)
-        pushgw=$pushgw_base/instance/bookinfo
-        rps=3000
+        PUSHGW=$pushgw_base/instance/bookinfo
+        RPS=3000
         app_instance_count=$(kubectl get namespaces | grep bookinfo | wc -l)
         ;;
     *)
@@ -23,25 +23,34 @@ case "$app" in
 esac
 
 echo "App: $app"
-echo "RPS: $rps"
+[ -n "$2" ] && RPS="$2"
+echo "RPS: $RPS"
 
-[ -n "$2" ] && duration="$2"
-echo "Duration: ${duration}s"
+[ -n "$3" ] && DURATION="$3"
+echo "Duration: ${DURATION}s"
 
-[ -n "$3" ] && connections="$2"
-echo "Connections/Threads: ${connections}"
+[ -n "$4" ] && CONNECTIONS="$4"
+echo "Connections/Threads: ${CONNECTIONS}"
 
 script_location="$(dirname "${BASH_SOURCE[0]}")"
 
-kubectl run wrk2-prometheus \
-            --restart=Never \
-            --image=quay.io/kinvolk/wrk2-prometheus \
-            --overrides='{ "apiVersion": "v1", "spec": {"tolerations": [ {"key":"load-generator-node", "operator":"Exists", "effect": "NoSchedule" } ] } }' \
-                -- \
-            -p $pushgw \
-            -c $connections \
-            -d $duration \
-            -r $rps \
-            $($script_location/render_endpoints.sh \
-                            $script_location/endpoints-$app.txt.tmpl \
-                            $app_instance_count)
+ENDPOINTS=""
+for count in $(seq $app_instance_count); do
+    INSTANCE="$count"
+    export INSTANCE
+    if [ -n "$ENDPOINTS" ]; then
+        ENDPOINTS="$(echo "$ENDPOINTS";
+                     envsubst <"$script_location/endpoints-$app.txt.tmpl")" 
+    else
+        ENDPOINTS="$(envsubst <"$script_location/endpoints-$app.txt.tmpl")" 
+    fi
+done
+
+export PUSHGW CONNECTIONS DURATION RPS ENDPOINTS
+envsubst < "$script_location/wrk2-prometheus.yaml.tmpl" > wrk2-prometheus.yaml
+
+echo "YAML written to 'wrk2-prometheus.yaml'. Now deploying."
+
+kubectl apply -f wrk2-prometheus.yaml
+
+echo "Done."
