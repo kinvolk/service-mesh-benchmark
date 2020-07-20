8d3afba24f8e3129261a96ebbde1a9b6f585caab
Thilo Fromm <thilo@kinvolk.io>
2020-07-20 16:05:18 +0200

    metrics-merger: push latency summary for all runs, add dashboard
    Signed-off-by: Thilo Fromm <thilo@kinvolk.io>

--- metrics-merger/merger.py
+++ metrics-merger/merger.py
@@ -1,53 +1,89 @@
 #!/usr/bin/env python3
 
-# pip install prometheus-http-client
+# pip install prometheus-http-client (for pulling metrics)
 # export PROMETHEUS_URL='http://url:port' (default: http://localhost:9090)
+#
+# pip install prometheus-client (for publishing metrics to push gateway)
+#
+
 
 import json
+from collections import OrderedDict
+
 from prometheus_http_client import Prometheus
+from prometheus_client import CollectorRegistry, Gauge, push_to_gateway
 
 p = Prometheus()
 
+def metrics_iter(p, query):
+    m = p.query(metric=query)
+    j = json.loads(m)
+    return iter(j.get("data",{}).get("result"))
+# --
+
 def get_completed_runs(p):
     ret=[]
-    metric=json.loads(
-                     p.query(metric='wrk2_benchmark_progress{status="done"}'))
-    for i in metric.get("data",{}).get("result"):
+
+    for i in metrics_iter(p, 'wrk2_benchmark_progress{status="done"}'):
        ret.append(i["metric"]["run"])
 
     return ret
 # --
 
 def get_latency_histogram(run,detailed=False):
-    ret={}
-    d_s=""
+    ret=OrderedDict()
+
     if detailed:
-        d_s="detailed_"
-    metric=json.loads(
-            p.query(metric='wrk2_benchmark_latency_%sms{run="%s"}' %(d_s,run,)))
-    for i in metric.get("data",{}).get("result"):
+        detailed="detailed_"
+    else:
+        detailed=""
+
+    for i in metrics_iter(p,
+                    'wrk2_benchmark_latency_%sms{run="%s"}' %(detailed,run,)):
        ret[float(i["metric"]["p"])] = float(i["value"][1])
 
     return ret
 # --
 
+hist = {}
+det_hist = {}
 
+# put latency values of all runs into a dict indexed by latencies' percentile
 for run in get_completed_runs(p):
-    print("%s:\n"%(run,))
- 
-    print("    histogram\n")
     x = get_latency_histogram(run)
-    percs = sorted(x.items(), key=lambda item: item[1])
-    for perc,lat in percs:
-        print("    %f: %fms" % (perc, lat))
-    
-    print("    ----\n")
+    for perc,lat in x.items():
+        if hist.get(perc):
+            hist[perc][run]=lat
+        else:
+            hist[perc] = OrderedDict({run:lat})
 
-    print("    detailed histogram\n")
     x = get_latency_histogram(run,"detailed")
-    percs = sorted(x.items(), key=lambda item: item[1])
-    for perc,lat in percs:
-        print("    %f: %fms" % (perc, lat))
-    print("----")
+    for perc,lat in x.items():
+        if det_hist.get(perc):
+            det_hist[perc][run]=lat
+        else:
+            det_hist[perc] = {run:lat}
 
 
+# Now calculate "incremental" latency values that stack in a bar chart
+r = CollectorRegistry()
+gdiff = Gauge('wrk2_benchmark_summary_latency_ms_diff',
+          'latency summary (all runs)',
+            labelnames=["p","source_run"],
+            registry=r)
+g = Gauge('wrk2_benchmark_summary_latency_ms',
+          'latency summary (all runs)',
+            labelnames=["p","source_run"],
+            registry=r)
+for perc in hist.keys():
+    hist[perc] = {k: v for k, v in 
+                        sorted(hist[perc].items(), key=lambda item: item[1])}
+
+    prev=0
+    for run, lat in hist[perc].items():
+        gdiff.labels(p=perc, source_run=run).set(lat - prev)
+        g.labels(p=perc, source_run=run).set(lat)
+        prev = lat
+
+push_to_gateway('localhost:9091', job='bare-metal',
+        grouping_key={"instance":"emojivoto",}, registry=r)
